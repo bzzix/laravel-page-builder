d3e08d00d5f9d15017a25cc9a797c4b280a4c96e
Mahmoud Hassan <betalamoud@gmail.com>
2025-06-04 20:24:42 +0300

    Updatet plugins (config & index)

--- config/bzzix-pagebuilder.php
+++ config/bzzix-pagebuilder.php
@@ -3,4 +3,65 @@ return [
     'middleware'      => ['web'],
     'create_route'    => 'grapesjs/new',
     'update_route'    => 'grapesjs/update/{slug}',
+    
+    // روابط ملفات CSS
+    'styles' => [
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+    ],
+
+    // روابط ملفات JavaScript
+    'scripts' => [
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js',
+    ],
+
+    // الإضافات (plugins)
+    'plugins' => [
+        [
+            'name' => 'gjs-preset-webpage',
+            'enabled' => true,
+            'style' => '',
+            'script' => 'https://unpkg.com/grapesjs-preset-webpage',
+            'options' => []
+        ],
+        [
+            'name' => 'grapesjs-plugin-forms',
+            'enabled' => false, 
+            'style' => '',
+            'script' => 'https://unpkg.com/grapesjs-plugin-forms',
+            'options' => []
+        ],
+        [
+            'name' => 'grapesjs-navbar',
+            'enabled' => false,
+            'style' => '',
+            'script' => 'https://unpkg.com/grapesjs-navbar',
+            'options' => []
+        ],
+        [
+            'name' => 'grapesjs-custom-code',
+            'enabled' => false,
+            'style' => '',
+            'script' => 'https://unpkg.com/grapesjs-custom-code',
+            'options' => []
+        ],
+        [
+            'name' => 'grapesjs-tabs',
+            'enabled' => false,
+            'style' => 'https://unpkg.com/grapesjs-tabs/dist/grapesjs-tabs.min.css',
+            'script' => 'https://unpkg.com/grapesjs-tabs',
+            'options' => [
+                'tabsBlock' => []
+            ]
+        ],
+        [
+            'name' => 'gjs-blocks-basic',
+            'enabled' => true,
+            'style' => '',
+            'script' => 'https://unpkg.com/grapesjs-blocks-basic',
+                'options' => [
+                'blocks' => ['text', 'link', 'image', 'video', 'map'],
+            ]
+        ]
+    ],
+
 ];
--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -2,17 +2,92 @@
 <html>
 <head>
     <title>Page Builder</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    {{-- CSS الأساسي --}}
     <link href="https://unpkg.com/grapesjs/dist/css/grapes.min.css" rel="stylesheet">
-</head>
+    
+  @php
+      $plugins = collect(config('bzzix-pagebuilder.plugins', []))
+          ->where('enabled', true);
+
+      $pluginStyles = $plugins->pluck('style')->filter()->unique();
+      $pluginScripts = $plugins->pluck('script')->filter()->unique();
+      $pluginNames = $plugins->pluck('name')->filter()->toArray();
+
+      $pluginOptions = [];
+      foreach ($plugins as $plugin) {
+          $pluginOptions[$plugin['name']] = $plugin['options'] ?? [];
+      }
+  @endphp
+
+    {{-- تحميل CSS للبلجنات من config --}}
+    @foreach ($pluginStyles as $style)
+        <link rel="stylesheet" href="{{ $style }}">
+    @endforeach
+
+    <style>
+        body { margin: 0; }
+        .gjs-pn-panel.gjs-pn-commands {
+            display: flex;
+            justify-content: space-between;
+        }
+    </style>
+
+  </head>
 <body>
     <div id="gjs" style="height:100vh;"></div>
 
     <script src="https://unpkg.com/grapesjs"></script>
+
+    {{-- Scripts للبلجنات من config --}}
+    @foreach ($pluginScripts as $script)
+        <script src="{{ $script }}"></script>
+    @endforeach
+
     <script>
-        grapesjs.init({
-            container: '#gjs',
-            components: '<h1>Hello Page Builder</h1>',
-        });
+
+      const pluginNames = @json($pluginNames);
+      const pluginOptions = @json($pluginOptions);
+
+      const editor = grapesjs.init({
+          container: '#gjs',
+          fromElement: false,
+          height: '100vh',
+          storageManager: false,
+          components: '<h1>Hello Page Builder</h1>',
+          canvas: {
+              styles: {!! json_encode(config('bzzix-pagebuilder.styles', [])) !!},
+              scripts: {!! json_encode(config('bzzix-pagebuilder.scripts', [])) !!}
+          },
+          plugins: pluginNames,
+          pluginsOpts: pluginOptions,
+      });
+
+      editor.Commands.add('save-page', {
+        run(editor) {
+          const html = editor.getHtml();
+          const css = editor.getCss();
+          const json = editor.getComponents();
+
+          fetch('/api/pages', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+            },
+            body: JSON.stringify({
+              html: html,
+              css: css,
+              components: json
+            })
+          }).then(res => res.json()).then(data => {
+            alert('تم الحفظ بنجاح');
+          });
+        }
+      });
+
     </script>
 </body>
 </html>
+
